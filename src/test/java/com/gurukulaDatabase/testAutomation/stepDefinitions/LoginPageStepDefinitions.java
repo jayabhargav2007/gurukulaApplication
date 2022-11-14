c09d1acd7ec5483fa6333051560d2bc664a7e10e
jayabhargav2007 <73937278+jayabhargav2007@users.noreply.github.com>
2022-11-14 05:56:46 +0100

    Update LoginPageStepDefinitions.java

--- src/test/java/com/gurukulaDatabase/testAutomation/stepDefinitions/LoginPageStepDefinitions.java
+++ src/test/java/com/gurukulaDatabase/testAutomation/stepDefinitions/LoginPageStepDefinitions.java
@@ -11,7 +11,7 @@ import cucumber.api.java.en.When;
 import net.thucydides.core.annotations.Steps;
 import static org.junit.Assert.assertEquals;
 /**
- * Created by Srinivas Budha on 9/24/2018.
+ * Created by Bhargav on 9/24/2018.
  */
 public class LoginPageStepDefinitions {
     @Steps
